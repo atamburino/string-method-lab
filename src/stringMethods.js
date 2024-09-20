01b38d887dbaa0646bd46a36b69e31aefc5dea73
Andy Tamburino <andytamburino@gmail.com>
2024-09-19 22:15:54 -0400

    wired up fist solution for test and added fuction placeholders to run test

---
+++ src/stringMethods.js
@@ -0,0 +1,68 @@
+
+
+// charAt
+// Return the index
+// return string info on number
+// return string is empy on empty string
+function getCharAt (string, index) {
+    if (string.length === 0) {
+        return 'String is empty';
+    }
+    else if (typeof string !== 'string'){
+        return 'Value given to function was of Data Type: number';
+    }
+    else {
+        return string.charAt(index);
+    }
+}
+
+function concatenateStrings () {
+    return "hello";
+}
+
+function doesInclude () {
+    return "hello";
+}
+
+function getIndexOf () {
+    return "hello";
+}
+
+function sliceString () {
+    return "hello";
+}
+
+function splitString () {
+    return "hello";
+}
+
+function convertToLowerCase () {
+    return "hello";
+}
+
+function convertToUpperCase () {
+    return "hello";
+}
+
+function trimString () {
+    return "hello";
+}
+
+function replaceSubstring () {
+    return "hello";
+}
+
+module.exports = {
+    getCharAt,
+    concatenateStrings,
+    doesInclude,
+    getIndexOf,
+    sliceString,
+    splitString,
+    convertToLowerCase,
+    convertToUpperCase,
+    trimString,
+    replaceSubstring
+};
+
+
