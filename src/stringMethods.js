aa435f21b34c84f63a7ad3be65c60fba6c6d4124
Andy Tamburino <andytamburino@gmail.com>
2024-09-19 23:02:09 -0400

    finalized the concat tests

--- src/stringMethods.js
+++ src/stringMethods.js
@@ -16,41 +16,49 @@ function getCharAt (string, index) {
     }
 }
 
-function concatenateStrings () {
-    return "hello";
+function concatenateStrings (string1, string2) {
+    // Check if string1 is not a string
+    if (typeof string1 !== 'string') {
+        return `Value given to function was of Data Type: ${typeof string1}`;
+    }
+    // Check if string2 is not a string
+    if (typeof string2 !== 'string') {
+        return `Value given to function was of Data Type: ${typeof string2}`;
+    }
+    return string1.concat(string2)
 }
 
-function doesInclude () {
-    return "hello";
-}
+// function doesInclude () {
+//     return "hello";
+// }
 
-function getIndexOf () {
-    return "hello";
-}
+// function getIndexOf () {
+//     return "hello";
+// }
 
-function sliceString () {
-    return "hello";
-}
+// function sliceString () {
+//     return "hello";
+// }
 
-function splitString () {
-    return "hello";
-}
+// function splitString () {
+//     return "hello";
+// }
 
-function convertToLowerCase () {
-    return "hello";
-}
+// function convertToLowerCase () {
+//     return "hello";
+// }
 
-function convertToUpperCase () {
-    return "hello";
-}
+// function convertToUpperCase () {
+//     return "hello";
+// }
 
-function trimString () {
-    return "hello";
-}
+// function trimString () {
+//     return "hello";
+// }
 
-function replaceSubstring () {
-    return "hello";
-}
+// function replaceSubstring () {
+//     return "hello";
+// }
 
 module.exports = {
     getCharAt,
